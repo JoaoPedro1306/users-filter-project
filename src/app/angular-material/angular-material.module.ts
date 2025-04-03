e66d9d5282735685808745c26edcae84c6c02388
João Pedro <jaopedro_santos@hotmail.com>
2025-04-03 19:48:42 -0300

    Criação do Layout do Componente de Detalhes

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from "@angular/core";
+import { MatDividerModule } from "@angular/material/divider";
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+        MatListModule,
+        MatDividerModule
+    ],
+    exports: [
+        MatListModule,
+        MatDividerModule,
+    ],
 })
 export class AngularMaterialModule { }
