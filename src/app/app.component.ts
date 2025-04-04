ad757c42e34562dda8d77bbd5582b951ecd4ea67
João Pedro <jaopedro_santos@hotmail.com>
2025-04-04 02:18:51 -0300

    Passagem da lista de usuários via Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
 
@@ -7,10 +7,17 @@ import { UsersList } from './data/users-list';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  userSelected: IUser = {} as IUser;
+export class AppComponent implements OnInit {
+  usersList : IUser[] = [];
   showUserDetails: boolean = false;
-
+  userSelected: IUser = {} as IUser;
+  
+  ngOnInit() {
+    setTimeout(() => {
+      this.usersList = UsersList;
+    }, 2000);
+  }
+  
   onUserSelected(user: IUser){
     this.showUserDetails = true;
     this.userSelected = user;
